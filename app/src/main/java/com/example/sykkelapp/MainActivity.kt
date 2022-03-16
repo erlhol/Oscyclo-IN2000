c2840b6909fa38915f9bcb362b41b3e19ef95123
Erling Holte <erlinhol@uio.no>
2022-03-16 19:39:40 +0100

    Added API-call test

--- app/src/main/java/com/example/sykkelapp/MainActivity.kt
+++ app/src/main/java/com/example/sykkelapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sykkelapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,9 +14,8 @@ import com.example.sykkelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-
     private lateinit var binding: ActivityMainBinding
+    private val viewModel : MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +40,10 @@ class MainActivity : AppCompatActivity() {
         myButton.setOnClickListener {
             val intent = Intent(this,MapsActivity::class.java)
             startActivity(intent)
+        }
+
+        viewModel.geometry.observe(this) {
+                geometry -> println(geometry)
         }
     }
 }
