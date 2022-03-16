fae2f6a7e0ba3faac35a31ba4d12cbec43efe2f8
Erling Holte <erlinhol@uio.no>
2022-03-16 18:06:20 +0100

    Update ViewModel

--- app/src/main/java/com/example/sykkelapp/MainActivityViewModel.kt
+++ app/src/main/java/com/example/sykkelapp/MainActivityViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.sykkelapp
 
-class MainActivityViewModel{
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sykkelapp.data.Datasource
+import com.example.sykkelapp.data.locationForecast.Geometry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel : ViewModel() {
+
+    private val _geometry = MutableLiveData<Geometry>()
+    val geometry : LiveData<Geometry>
+        get() = _geometry
+
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            _geometry.postValue(Datasource().loadWheather())
+        }
+    }
 }
