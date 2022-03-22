d4bce13ae0edc91f5bd0f44dd209d2c2eeee607e
Erling Holte <erlinhol@uio.no>
2022-03-22 12:30:57 +0100

    Remove unused import

--- app/src/main/java/com/example/sykkelapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sykkelapp/ui/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.example.sykkelapp.ui.home
 
-import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.graphics.toColorInt
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.sykkelapp.R
@@ -15,8 +13,6 @@ import com.example.sykkelapp.databinding.FragmentHomeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.data.geojson.GeoJsonLayer
