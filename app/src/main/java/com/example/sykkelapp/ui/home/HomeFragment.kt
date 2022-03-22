e01d84ebc313dfb17f3d89695778d433dbff9a86
Erling Holte <erlinhol@uio.no>
2022-03-22 12:25:14 +0100

    Added pointers (stations) for Air Quality

--- app/src/main/java/com/example/sykkelapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sykkelapp/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.sykkelapp.ui.home
 
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.graphics.toColorInt
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.sykkelapp.R
@@ -13,6 +15,8 @@ import com.example.sykkelapp.databinding.FragmentHomeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.data.geojson.GeoJsonLayer
@@ -64,6 +68,17 @@ class HomeFragment : Fragment() {
                         Toast.makeText(context, it.id, Toast.LENGTH_SHORT).show()
                     }
             }
+
+            homeViewModel.air.observe(viewLifecycleOwner) { list ->
+                list.forEach {
+                    val point = LatLng(it.latitude,it.longitude)
+                    mMap.addMarker(MarkerOptions()
+                        .position(point)
+                        .title(it.station + it.value + it.unit)
+                    )
+                }
+            }
+
         }
 
         homeViewModel.data.observe(viewLifecycleOwner) {
