bfdfb1f0421d8753a4c65b61a464efbf820c8cdd
Erling Holte <erlinhol@uio.no>
2022-03-22 17:58:32 +0100

    Currently fixed RecyclerView

--- app/src/main/java/com/example/sykkelapp/ui/dashboard/DashboardAdapter.kt
+++ app/src/main/java/com/example/sykkelapp/ui/dashboard/DashboardAdapter.kt
@@ -1,44 +1,57 @@
 package com.example.sykkelapp.ui.dashboard
 
-import android.content.ReceiverCallNotAllowedException
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.sykkelapp.R
-import com.example.sykkelapp.data.geoserver.Geometry
 import com.example.sykkelapp.ui.Path
 
+class DashboardAdapter(private val exampleList: List<Path>) : RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
 
-abstract class DashboardAdapter(private val exampleList: List<Path>) : RecyclerView.Adapter<DashboardAdapter.ViewHolder>(){
-
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView : ImageView = view.findViewById(R.id.picture)
-        val lenghtNr: TextView = view.findViewById(R.id.lengthNr)
-        val airQImg: ImageView = view.findViewById(R.id.airQ)
-        val difficulty: TextView = view.findViewById(R.id.diff)
+        val imageView: ImageView
+        val lengthNr : TextView
+        val airQImg : TextView
+        val difficulty: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            imageView = view.findViewById(R.id.picture)
+            lengthNr = view.findViewById(R.id.lengthNr)
+            airQImg = view.findViewById(R.id.airQ)
+            difficulty = view.findViewById(R.id.diff)
+        }
     }
 
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.element, viewGroup, false)
 
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(view)
+    }
 
-            val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.element, viewGroup, false)
-            return ViewHolder(view)
-        }
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
-        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            val currentItem = exampleList[position]
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
 
-            //Glide.with(holder.imageView).load(currentItem).into(holder.imageView)
-            //holder.lengthNr = currentItem[position]
-            //Glide.with(holder.airQImg).load(currentItem).into(holder.airQImg)
-            //holder.difficulty = currentItem[position]
-        }
+        // viewHolder.imageView = exampleList[position].
+        viewHolder.lengthNr.text = exampleList[position].length.toString()
+        viewHolder.difficulty.text = exampleList[position].difficulty.toString()
+        //Glide.with(holder.airQImg).load(currentItem).into(holder.airQImg)
+        //Glide.with(holder.imageView).load(currentItem).into(holder.imageView)
+    }
 
-        // Return the size of your dataset (invoked by the layout manager)
-
-
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = exampleList.size
 }
--- app/src/main/java/com/example/sykkelapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/sykkelapp/ui/dashboard/DashboardFragment.kt
@@ -1,20 +1,17 @@
 package com.example.sykkelapp.ui.dashboard
 
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.sykkelapp.MainActivity
 import com.example.sykkelapp.R
 import com.example.sykkelapp.databinding.FragmentDashboardBinding
+import com.example.sykkelapp.ui.Path
 
 class DashboardFragment : Fragment(), AdapterView.OnItemSelectedListener {
     private lateinit var binding : FragmentDashboardBinding
@@ -29,7 +26,7 @@ class DashboardFragment : Fragment(), AdapterView.OnItemSelectedListener {
             ViewModelProvider(this).get(DashboardViewModel::class.java)
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        /*
+
         val spinner: Spinner = binding.spinner
         binding.spinner.onItemSelectedListener = this
         context?.let {
@@ -40,7 +37,12 @@ class DashboardFragment : Fragment(), AdapterView.OnItemSelectedListener {
                 spinner.adapter = adapter
             }
         }
-         */
+        // Just one example, has to be changed to MVVM
+        val recyclerView = binding.recyclerView
+        var list = mutableListOf<Path>()
+        val p = Path(500.0,3,9)
+        list.add(p)
+        recyclerView.adapter = DashboardAdapter(list)
 
         return root
     }
@@ -50,10 +52,10 @@ class DashboardFragment : Fragment(), AdapterView.OnItemSelectedListener {
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        TODO("Not yet implemented")
+        println("hei")
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
-        TODO("Not yet implemented")
+
     }
 }
