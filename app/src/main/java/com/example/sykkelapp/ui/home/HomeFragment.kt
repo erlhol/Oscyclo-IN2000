678a586abd83c9367d4f32c3728aeb211efe41ed
Uvejs Sadiki <uvejss@ulrik.uio.no>
2022-03-22 11:35:19 +0100

    Element.xml

--- app/src/main/java/com/example/sykkelapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sykkelapp/ui/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.sykkelapp.ui.home
 
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.sykkelapp.MainActivity
 import com.example.sykkelapp.R
 import com.example.sykkelapp.databinding.FragmentHomeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
