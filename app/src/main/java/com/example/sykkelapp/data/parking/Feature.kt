4b15ab723e621e8de290c0e5d8e7d59300928108
Erling Holte <erlinhol@uio.no>
2022-03-30 18:26:01 +0200

    Remove redundant data class

--- app/src/main/java/com/example/sykkelapp/data/parking/Feature.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sykkelapp.data.parking
-
-data class Feature(
-    val geometry: Geometry,
-    val geometry_name: String,
-    val id: String,
-    val properties: PropertiesX,
-    val type: String
-)
