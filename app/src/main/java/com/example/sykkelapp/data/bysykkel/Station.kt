6f4b97116d8dac7de668b6a1a7ab87f42d56d340
Edis Kukuruzovic <edisk@ulrik.uio.no>
2022-04-02 12:59:58 +0200

    Bysykkel station

--- app/src/main/java/com/example/sykkelapp/data/bysykkel/Station.kt
+++ app/src/main/java/com/example/sykkelapp/data/bysykkel/Station.kt
@@ -1,5 +1,8 @@
 package com.example.sykkelapp.data.bysykkel
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
+
 data class Station(
     val address: String,
     val capacity: Int,
@@ -7,5 +10,32 @@ data class Station(
     val lon: Double,
     val name: String,
     val rental_uris: RentalUris,
-    val station_id: String
-)
+    val station_id: String,
+    val latlng: LatLng = LatLng(lat,lon)
+) : ClusterItem {
+
+    override fun getPosition(): LatLng {
+        return latlng
+    }
+
+    override fun getTitle(): String? {
+        return name
+    }
+
+    override fun getSnippet(): String? {
+        return address
+    }
+}
+
+/*
+override fun getPosition(): LatLng =
+    latLng
+
+override fun getTitle(): String =
+    name
+
+override fun getSnippet(): String =
+    address
+}
+
+ */
