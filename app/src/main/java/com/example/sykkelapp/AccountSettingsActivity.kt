093c1e5f2df0654f99c40cd202aab4b1cb860c8f
Jehan Khodabocus <jehank@uio.no>
2022-04-21 18:26:16 +0200

    Add profile logic WIP

--- app/src/main/java/com/example/sykkelapp/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/sykkelapp/AccountSettingsActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sykkelapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sykkelapp.databinding.ActivityAccountSettingsBinding
+import com.example.sykkelapp.databinding.ActivityEditAccountBinding
+import com.example.sykkelapp.ui.profile.ProfileFragment
 
 class AccountSettingsActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityAccountSettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_settings)
+        _binding = ActivityAccountSettingsBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        _binding.settingsEditButton.setOnClickListener{
+            startActivity(Intent(this, EditAccountActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/sykkelapp/EditAccountActivity.kt
+++ app/src/main/java/com/example/sykkelapp/EditAccountActivity.kt
@@ -1,11 +1,24 @@
 package com.example.sykkelapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sykkelapp.databinding.ActivityEditAccountBinding
+import com.example.sykkelapp.ui.profile.ProfileFragment
 
 class EditAccountActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityEditAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_account)
+        _binding = ActivityEditAccountBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        _binding.saveButton.setOnClickListener{
+            val intent = Intent(this@EditAccountActivity, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            finish()
+        }
     }
 }
