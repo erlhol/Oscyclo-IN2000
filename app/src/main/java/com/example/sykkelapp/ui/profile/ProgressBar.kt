4bec90c49d3b835057a997ae3f78b7535dcb2dbb
Jehan Khodabocus <jehank@uio.no>
2022-05-01 12:02:19 +0200

    Add Progress bar

---
+++ app/src/main/java/com/example/sykkelapp/ui/profile/ProgressBar.kt
@@ -0,0 +1,12 @@
+package com.example.sykkelapp.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sykkelapp.R
+
+class ProgressBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress_bar)
+    }
+}
