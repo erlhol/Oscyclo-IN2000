4b43752a1a48367f7d0b8b4652e2f1d8eb0fc478
Jehan Khodabocus <jehank@uio.no>
2022-05-03 11:37:51 +0200

    Update profile fragment

--- app/src/main/java/com/example/sykkelapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/sykkelapp/ui/profile/ProfileFragment.kt
@@ -1,22 +1,14 @@
 package com.example.sykkelapp.ui.profile
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.sykkelapp.MainActivity
 import com.example.sykkelapp.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class ProfileFragment : Fragment() {
 
@@ -37,17 +29,9 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // Initialize Firebase Auth
-        //firebaseAuth = FirebaseAuth.getInstance()
-        //loadUserInfo()
-
         _binding!!.profileSettingsButton.setOnClickListener{
             startActivity(Intent(context, AccountSettingsActivity::class.java))
         }
-//        val textView: TextView = binding.profileFragmentUsername
-//        profileViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
         return root
     }
 
@@ -59,59 +43,6 @@ class ProfileFragment : Fragment() {
             startActivity(Intent(context, SignInActivity::class.java))
         }
     }
-//
-//    private fun createAccount{
-//        firebaseAuth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                    // Sign in success, update UI with the signed-in user's information
-//                    Log.d(TAG, "createUserWithEmail:success")
-//                    val user = firebaseAuth.currentUser
-//                    updateUI(user)
-//                } else {
-//                    // If sign in fails, display a message to the user.
-//                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-//                    Toast.makeText(baseContext, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show()
-//                    updateUI(null)
-//                }
-//            }
-//    }
-//
-//
-//    private fun signIn(){
-//        firebaseAuth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                    // Sign in success, update UI with the signed-in user's information
-//                    Log.d(TAG, "signInWithEmail:success")
-//                    val user = firebaseAuth.currentUser
-//                    updateUI(user)
-//                } else {
-//                    // If sign in fails, display a message to the user.
-//                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-//                    Toast.makeText(baseContext, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show()
-//                    updateUI(null)
-//                }
-//            }
-//    }
-//
-//    private fun loadUserInfo() {
-//        val ref = FirebaseDatabase.getInstance().getReference("Users")
-//        firebaseAuth.uid?.let {
-//            ref.child(it)
-//                .addValueEventListener(object : ValueEventListener {
-//                    override fun onDataChange(snapshot: DataSnapshot){
-//
-//                    }
-//
-//                    override fun onCancelled(error: DatabaseError) {
-//                        TODO("Not yet implemented")
-//                    }
-//                })
-//        }
-//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
