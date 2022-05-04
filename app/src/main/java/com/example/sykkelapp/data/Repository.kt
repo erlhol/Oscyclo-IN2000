04742d0e76329a5f705ea0e1eaf0dabeeb7e1d5f
Erling Holte <erlinhol@uio.no>
2022-05-04 13:11:53 +0200

    Add try-catch

--- app/src/main/java/com/example/sykkelapp/data/Repository.kt
+++ app/src/main/java/com/example/sykkelapp/data/Repository.kt
@@ -59,61 +59,86 @@ class Repository (private val datasource: Datasource) {
     }
 
     suspend fun loadBySykkelRoutes() : List<Route>? {
-        val res = datasource.loadBySykkelRoutes()
-        // create map of routes: Key = start,end. Value = BysykkelItem
-        val startEndMap = res.map {
-            Key(it.start_station_id,it.end_station_id) to it
-        }.toMap()
+        try {
+            val res = datasource.loadBySykkelRoutes()
+            // create map of routes: Key = start,end. Value = BysykkelItem
+            val popularRoutes = findPopularRoutes(res)
+            val jobsList = mutableListOf<Deferred<Int>>()
+            val routes = mutableListOf<Route>()
+
+            popularRoutes.forEach {
+                val job = CoroutineScope(Dispatchers.IO).async {
+                    val placeid = datasource.loadPlaceId(
+                        it.start_station_name,
+                        (it.start_station_latitude.toString() + "," + it.start_station_longitude)
+                    )
+                    val air_quality = averageAirQuality(
+                        it.start_station_latitude,
+                        it.start_station_longitude,
+                        it.end_station_latitude,
+                        it.end_station_longitude
+                    )
+                    val airq_unit = datasource.loadAirQualityForecast(
+                        it.start_station_latitude.toString(),
+                        it.start_station_longitude.toString()
+                    )
+                    val directions = datasource.getDirection(
+                        it.start_station_latitude.toString() + "," + it.start_station_longitude,
+                        it.end_station_latitude.toString() + "," + it.end_station_longitude
+                    )
+                    val elevation = findElevationDiff(
+                        it.start_station_latitude.toString() + "," + it.start_station_longitude,
+                        it.end_station_latitude.toString() + "," + it.end_station_longitude
+                    )
+                    routes.add(
+                        Route(
+                            it.end_station_description,
+                            it.end_station_id,
+                            it.end_station_latitude,
+                            it.end_station_longitude,
+                            it.end_station_name,
+                            it.start_station_description,
+                            it.start_station_id,
+                            it.start_station_latitude,
+                            it.start_station_longitude,
+                            it.start_station_name,
+                            placeid,
+                            air_quality,
+                            airq_unit.units,
+                            directions,
+                            it.popularity,
+                            elevation,
+                            setDifficulty(directions.legs[0])
+                        )
+                    )
+                    1
+                }
+                jobsList.add(job)
+            }
+            jobsList.awaitAll()
+
+            return routes.sortedByDescending { it.popularity } // way to sort recyclerview
+        }
+        catch (exception: Exception) {
+            return null
+        }
+    }
+
+    private fun findPopularRoutes(bysykkelItems: List<BysykkelItem>) : List<BysykkelItem> {
+        val startEndMap = bysykkelItems.associateBy {
+            Key(it.start_station_id, it.end_station_id)
+        }
 
         // aggregate the routes with same start_id and end_id and count it
-        val eachCountMap = res.groupingBy { Key(it.start_station_id,it.end_station_id) }.eachCount()
+        val eachCountMap = bysykkelItems.groupingBy { Key(it.start_station_id,it.end_station_id) }.eachCount()
 
         // add the most popular routes in a list. Just the 20 most popular
         var popularRoutes = mutableListOf<BysykkelItem>()
-        val routes = mutableListOf<Route>()
         eachCountMap.toList().sortedByDescending { it.second }.take(20).forEach {
             startEndMap[it.first]?.let { it1 -> popularRoutes.add(it1)
                 it1.popularity = it.second}
         }
-        val jobsList = mutableListOf<Deferred<Int>>()
-
-        popularRoutes.forEach {
-            val job = CoroutineScope(Dispatchers.IO).async {
-                val placeid = datasource.loadPlaceId(it.start_station_name,(it.start_station_latitude.toString()+","+it.start_station_longitude))
-                val air_quality = averageAirQuality(
-                    it.start_station_latitude,
-                    it.start_station_longitude,
-                    it.end_station_latitude,
-                    it.end_station_longitude)
-                val airq_unit = datasource.loadAirQualityForecast(it.start_station_latitude.toString(),it.start_station_longitude.toString())
-                val directions = datasource.getDirection(it.start_station_latitude.toString()+","+it.start_station_longitude,it.end_station_latitude.toString()+","+it.end_station_longitude)
-                val elevation = findElevationDiff(it.start_station_latitude.toString()+","+it.start_station_longitude,it.end_station_latitude.toString()+","+it.end_station_longitude)
-                routes.add(
-                    Route(
-                    it.end_station_description,
-                    it.end_station_id,
-                    it.end_station_latitude,
-                    it.end_station_longitude,
-                    it.end_station_name,
-                    it.start_station_description,
-                    it.start_station_id,
-                    it.start_station_latitude,
-                    it.start_station_longitude,
-                    it.start_station_name,
-                    placeid,
-                    air_quality,
-                    airq_unit.units,
-                    directions,
-                    it.popularity,
-                    elevation,
-                    setDifficulty(directions.legs[0]))
-                )
-                1
-            }
-            jobsList.add(job)
-        }
-        jobsList.awaitAll()
-        return routes.sortedByDescending { it.popularity } // way to sort recyclerview
+        return popularRoutes
     }
 
     suspend fun findElevationDiff(destlatlon: String, originlatlon: String): Double {
